e777fa5c13a4ed87d494f43a65f51efa0e441209
Alejandro Zárate <102533262+AlejandroZarateGamarra@users.noreply.github.com>
2024-05-03 03:56:44 -0500

    fix: se actualizó la dependencia para instalar angular material

--- src/app/views/catalogue/catalogue/catalogue.component.ts
+++ src/app/views/catalogue/catalogue/catalogue.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import {EventsComponent} from "../events/events.component";
 import {PostComponent} from "../../../components/post/post.component";
+import {MatButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 
 @Component({
   selector: 'app-catalogue',
   standalone: true,
-  imports: [
-    EventsComponent,
-    PostComponent
-  ],
+    imports: [
+        EventsComponent,
+        PostComponent,
+        MatButton,
+        RouterLink
+    ],
   templateUrl: './catalogue.component.html',
   styleUrl: './catalogue.component.scss'
 })
